6bcd6fea817034f716f356d59621952dd7573f03
wcdennis38 <myaws6000@gmail.com>
2026-06-12 23:14:48 +0000

    Fix Terraform syntax errors and clean config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
     random = {
-      source  = "hashicorp/random"
+      source = "hashicorp/random"
     }
   }
 }
@@ -15,11 +15,6 @@ provider "aws" {
 
 resource "random_id" "suffix" {
   byte_length = 4
-}
-
-resource "aws_s3_bucket" "devsecops_bucket" {
-  bucket = "devsecops-demo-bucket-${random_id.suffix.hex}"
-}
 }
 
 resource "aws_s3_bucket" "devsecops_bucket" {
