4b53a6c2b84b7fda544a992349de03e4440836ea
Dennis WC <myaws6000@gmail.com>
2026-06-13 05:34:36 +0000

    Fix CodeBuild buildspec for Terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-terraform-state-waynedennis-001"
-    key    = "devsecops/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "devsecops-terraform-state-wc-dennis-001"
+    key     = "devsecops/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
