0d615edd2e5eb674e8ebd68e992ab054317ebac3
wcdennis38 <myaws6000@gmail.com>
2026-06-28 19:22:09 -0400

    Remove S3 backend configuration from backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,3 @@
 terraform {
   required_version = ">= 1.6.6"
-
-  backend "s3" {
-    bucket         = "REPLACE_WITH_REAL_BUCKET"
-    key            = "devsecops-pipeline/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "REPLACE_WITH_REAL_LOCK_TABLE"
-    encrypt        = true
-  }
 }
