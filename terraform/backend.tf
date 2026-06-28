35498412fd04d3e0cf4044352303a0c05bb27a4a
wcdennis38 <myaws6000@gmail.com>
2026-06-27 23:49:13 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = ">= 1.6.6"
+
+  backend "s3" {
+    bucket         = "YOUR-TERRAFORM-STATE-BUCKET"
+    key            = "devsecops-pipeline/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
