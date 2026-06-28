b2111ab341a8969f38e594aa5c27d78922bcf084
wcdennis38 <myaws6000@gmail.com>
2026-06-27 21:56:41 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,31 @@
 import os
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+# Read Flask secret strictly from environment
+SECRET_KEY = os.getenv("FLASK_APP_KEY")
+if not SECRET_KEY:
+    raise RuntimeError("Missing required environment variable: FLASK_APP_KEY")
+
+app.config.update(
+    SECRET_KEY=SECRET_KEY
+)
+
+# Enable CSRF protection
+csrf = CSRFProtect(app)
+
+
 @app.route("/")
 def home():
-    return {"message": "DevSecOps app is running"}
+    return {"message": "DevSecOps Pipeline Running in AWS!"}
 
-# Optional health endpoint (good for Docker/CI)
+
 @app.route("/health")
 def health():
     return {"status": "healthy"}
+
 
 if __name__ == "__main__":
     HOST = os.getenv("HOST", "0.0.0.0")
