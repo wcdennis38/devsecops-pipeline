877b9dbce77ed42536e42d8ca34583ba947f9702
wcdennis38 <myaws6000@gmail.com>
2026-06-28 19:17:54 -0400

    Configure S3 backend for Terraform state management

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = ">= 1.6.6"
+
+  backend "s3" {
+    bucket         = "YOUR_TF_STATE_BUCKET_NAME"
+    key            = "devsecops-pipeline/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "YOUR_TF_LOCK_TABLE_NAME"
+    encrypt        = true
+  }
 }
