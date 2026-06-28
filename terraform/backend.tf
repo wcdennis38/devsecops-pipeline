cce9cd3841640226d7bff77e1baade08e2e0dfc3
wcdennis38 <myaws6000@gmail.com>
2026-06-28 19:20:06 -0400

    Update S3 bucket and DynamoDB table placeholders

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.6.6"
 
   backend "s3" {
-    bucket         = "YOUR_TF_STATE_BUCKET_NAME"
+    bucket         = "REPLACE_WITH_REAL_BUCKET"
     key            = "devsecops-pipeline/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "YOUR_TF_LOCK_TABLE_NAME"
+    dynamodb_table = "REPLACE_WITH_REAL_LOCK_TABLE"
     encrypt        = true
   }
 }
